b2cc472329c92190e4e4fc5d800ba30e90fee371
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-16 11:12:45 -0400

    starting the project now

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,32 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var sleepAmount = 8.0
   @State private var wakeUp = Date.now
+  @State private var sleepAmount = 8.0
+  @State private var coffeeAmount = 1
   
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-          Text("Hello, world!")
-          HStack {
-            Spacer()
-            Stepper("\(sleepAmount.formatted()) hours:", value: $sleepAmount, in: 4...12, step: 0.25)
-              .padding()
-            
-            Spacer()
-          }
-          DatePicker("Please enter date:", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
-            .labelsHidden()
-          DatePicker("Please enter date:", selection: $wakeUp,  displayedComponents: .hourAndMinute)
-            .labelsHidden()
-          
-          Text(Date.now, format: .dateTime.day().month().year().hour())
-          Text(Date.now.formatted(date: .long, time: .shortened))
-        }
-        .padding()
+  var body: some View {
+    NavigationStack {
+      VStack {
+        Text("When do you want to wake up?")
+          .font(.headline)
+        
+        DatePicker("Please enter a time: ", selection: $wakeUp, displayedComponents: .hourAndMinute)
+          .labelsHidden()
+      }
     }
+  }
 }
 
 #Preview {
